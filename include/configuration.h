8ebe126b5c718cf4f0e3ac1a4b2a28a8b39157a2
bobalu113 <bobalu113@panterasbox.com>
2014-07-17 03:15:35 -0700

    Uploading driver source and headers for reference purposes only. This is not the complete LDMud distribution, so it may or may not build. For the complete tarball, visit http://www.ldmud.eu/. This code was taken from LDMud version 3.3.720, and will hopefully remain unmodified until a new release.

---
+++ include/configuration.h
@@ -0,0 +1,15 @@
+#ifndef LPC_CONFIGURATION_H_
+#define LPC_CONFIGURATION_H_
+
+/* Definition of argument values for configure_interactive().
+ */
+
+/* Possible options for configure_interactive().
+ */
+#define IC_MAX_WRITE_BUFFER_SIZE 0
+
+/* Possible options for configure_driver().
+ */
+#define DC_MEMORY_LIMIT 0
+
+#endif /* LPC_CONFIGURATION_H_ */
---
+++ src/pkg-iksemel.h
@@ -0,0 +1,16 @@
+#ifndef PKG_IKSEMEL_H__
+#define PKG_IKSEMEL_H__ 1
+
+#include "driver.h"
+#include "machine.h"
+
+#if defined(USE_XML) && defined(HAS_IKSEMEL)
+
+/* --- Prototypes --- */
+
+void pkg_iksemel_init();
+
+#endif /* USE_XML && HAS_IKSEMEL */
+
+#endif /* PKG_IKSEMEL_H__ */
+
