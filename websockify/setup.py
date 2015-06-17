747ea1b2d26c4dbd49fff8d5d650065dc40768f7
Bobby Schaetzle <bobalu@panterasbox.com>
2015-06-16 17:56:31 -0700

    websockify

---
+++ websockify/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+version = '0.6.0'
+name = 'websockify'
+long_description = open("README.md").read() + "\n" + \
+    open("CHANGES.txt").read() + "\n"
+
+setup(name=name,
+      version=version,
+      description="Websockify.",
+      long_description=long_description,
+      classifiers=[
+          "Programming Language :: Python",
+        ],
+      data_files=[('share/websockify/include',
+                      ['include/util.js',
+                       'include/base64.js',
+                       'include/websock.js']),
+                  ('share/websockify/include/web-socket-js',
+                      ['include/web-socket-js/WebSocketMain.swf',
+                       'include/web-socket-js/swfobject.js',
+                       'include/web-socket-js/web_socket.js'])],
+      keywords='noVNC websockify',
+      license='LGPLv3',
+      url="https://github.com/kanaka/websockify",
+      author="Joel Martin",
+      author_email="github@martintribe.org",
+
+      packages=['websockify'],
+      include_package_data=True,
+      install_requires=['numpy'],
+      zip_safe=False,
+      entry_points={
+        'console_scripts': [
+            'websockify = websockify.websocketproxy:websockify_init',
+        ]
+      },
+    )
