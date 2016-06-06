17230f4ee690729f79b08cc4b49a1b89d3f2aa39
Bobby Schaetzle <bobalu@panterasbox.com>
2016-06-05 21:13:44 -0700

    created tracker service

---
+++ mudlib/platform/.obj/tracker_service.c
@@ -0,0 +1,30 @@
+/**
+ * A service for initializing and reloading tracker objects.
+ * 
+ * @author devo@eotl
+ * @alias TrackerService
+ */
+
+object reload_tracker(string tracker) {
+  object result = FINDO(tracker);
+  if (result) {
+    result = reload_object(result);
+  } else {
+    result = load_object(tracker);
+    result->init_tracker();
+  }
+  return result;
+}
+
+void load_trackers() {
+  reload_tracker(FileTracker);
+  reload_tracker(DomainTracker);
+  reload_tracker(ProgramTracker);
+  reload_tracker(ObjectTracker);
+  reload_tracker(ConnectionTracker);
+  reload_tracker(SessionTracker);
+}
+
+void create() {
+  load_trackers();
+}
